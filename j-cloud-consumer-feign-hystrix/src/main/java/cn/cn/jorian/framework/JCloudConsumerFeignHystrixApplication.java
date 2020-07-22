89f7e7c2e1685c7980e378feafc5575678fcbccd
ZhangXiaokun <1806520061@qq.com>
2020-07-22 14:42:54 +0800

    Spring-Cloud新增hystrix

---
+++ j-cloud-consumer-feign-hystrix/src/main/java/cn/cn/jorian/framework/JCloudConsumerFeignHystrixApplication.java
@@ -0,0 +1,13 @@
+package cn.cn.jorian.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JCloudConsumerFeignHystrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JCloudConsumerFeignHystrixApplication.class, args);
+    }
+
+}
